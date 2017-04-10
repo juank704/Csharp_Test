719db4625322024880c21713acce9b79c7d56ca7
juank704 <juan_k704@hotmail.com>
2017-04-10 00:39:12 -0400

    Commit 2 new Branch

--- SolutionApplication1/ConsoleApplication1/Program.cs
+++ SolutionApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            //AplicationBranch
+            //AplicationBranch2
             Console.WriteLine("Hello World");
         }
     }
